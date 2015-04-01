e9af4e33a6af178b4f6741d2e84b0296dac64fc2
Vinisha Hathiramani <vinishash@gmail.com>
2015-04-01 13:56:07 +0100

    animation added

--- app/assets/javascripts/controllers/vufinder-controller.js
+++ app/assets/javascripts/controllers/vufinder-controller.js
@@ -44,42 +44,41 @@ app.controller('VufinderController', ['$scope', '$http', '$timeout', function($s
 
   }
 
- // image upload
-  $scope.upload = function($timeout) {
-    // Configure The S3 Object 
-    AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
-    AWS.config.region = 'us-east-1';
-    var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
-   
-    if($scope.file) {
-      var params = { Key: $scope.file.name, ContentType: $scope.file.type, Body: $scope.file, ServerSideEncryption: 'AES256' };
-   
-      bucket.putObject(params, function(err, data) {
-        if(err) {
-          // There Was An Error With Your S3 Config
-          alert(err.message);
-          return false;
-        }
-        else {
-          // Success!
-          alert('Upload Done');
-        }
-      })
-      .on('httpUploadProgress',function(progress) {
-            // Log Progress Information
-            console.log(Math.round(progress.loaded / progress.total * 100) + '% done');
-          });
-    }
-    else {
-      // No File Selected
-      alert('No File Selected');
-    }
-      $scope.url = 'https://s3-eu-west-1.amazonaws.com/wonderwear/' + $scope.file.name
-    // $scope.getVueData($scope.url)
-    $scope.getImageData($scope.url)
-    $scope.dataReceived = true;
+// image upload
+$scope.upload = function($timeout) {
+  // Configure The S3 Object 
+  AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
+  AWS.config.region = 'us-east-1';
+  var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
+ 
+  if($scope.file) {
+    var params = { Key: $scope.file.name, ContentType: $scope.file.type, Body: $scope.file, ServerSideEncryption: 'AES256' };
+ 
+    bucket.putObject(params, function(err, data) {
+      if(err) {
+        // There Was An Error With Your S3 Config
+        alert(err.message);
+        return false;
+      }
+      else {
+        // Success!
+        alert('Upload Done');
+      }
+    })
+    .on('httpUploadProgress',function(progress) {
+          // Log Progress Information
+          console.log(Math.round(progress.loaded / progress.total * 100) + '% done');
+        });
   }
-
+  else {
+    // No File Selected
+    alert('No File Selected');
+  }
+    $scope.url = 'https://s3-eu-west-1.amazonaws.com/wonderwear/' + $scope.file.name
+  // $scope.getVueData($scope.url)
+  $scope.getImageData($scope.url)
+  $scope.dataReceived = true;
+}
 
   // shopsense API
   $scope.getPrice = function(clothingColor, clothingType) {
